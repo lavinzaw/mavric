5ab7f5cc7b9f15c7f540204d9041dc92386afa7d
lavinzaw <lavinzaw@gmail.com>
2022-03-08 22:32:37 +0530

    loginPage

---
+++ src/test/java/org/cate/SampleGit.java
@@ -0,0 +1,13 @@
+package org.cate;
+
+public class SampleGit {
+	public void products() {
+        	System.out.println("helen");
+     	}
+	public void products2() {
+		System.out.println("mcgorry");
+     	}
+	public void products3() {
+		System.out.println("thomas");
+	}
+}
